e2ef670b91b774e9cfd235839f7ae3e443240e0b
SheCode287 <ivy.muthoni.wanjohi@gmail.com>
2024-10-25 12:39:37 +0300

    changed TESLA file name

--- src/components/sidebars/Side.jsx
+++ src/components/sidebars/Side.jsx
@@ -6,7 +6,9 @@ import { MdPeople } from "react-icons/md";
 import { PiListChecksFill } from "react-icons/pi";
 import { IoBulb } from "react-icons/io5";
 import { IoSettingsSharp } from "react-icons/io5";
-import TeslaIcon from "../../assets/Tesla.svg";
+import TeslaIcon from "../../assets/TESLA.svg";
+import TeslaIcon from "../../../public/TESLA.svg";
+
 
 
 
